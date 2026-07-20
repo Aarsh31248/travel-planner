cbd4375eb9975d8635bc9751648d9276087c20d9
Aarsh Singh <starfire9599@gmail.com>
2026-07-20 21:13:10 +0530

    Added the image upload feature in new trips form through server actions

--- app/trips/new/page.tsx
+++ app/trips/new/page.tsx
@@ -4,10 +4,13 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { createTrip } from "@/lib/actions/create-trip";
 import { cn } from "@/lib/utils";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
+import { UploadButton } from "@/lib/upload-thing";
+import Image from "next/image";
 
 export default function NewTrip() {
   const [isPending, startTransition] = useTransition();
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
 
   return (
     <div className="max-w-lg mx-auto mt-10">
@@ -17,6 +20,9 @@ export default function NewTrip() {
           <form
             className="space-y-6"
             action={(formData: FormData) => {
+              if (imageUrl) {
+                formData.append("imageUrl", imageUrl);
+              }
               startTransition(() => {
                 createTrip(formData);
               });
@@ -33,7 +39,7 @@ export default function NewTrip() {
                 placeholder="Japan trip..."
                 className={cn(
                   "w-full border border-gray-300 px-3 py-2",
-                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 )}
                 required
               />
@@ -47,12 +53,12 @@ export default function NewTrip() {
                 placeholder="Trip description..."
                 className={cn(
                   "w-full border border-gray-300 px-3 py-2",
-                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 )}
                 required
               />
             </div>
-
+            
             <div className="grid grid-cols-2 gap-4">
               <div>
                 <label className="block text-sm font-medium text-gray-700 mb-1">
@@ -63,7 +69,7 @@ export default function NewTrip() {
                   name="startDate"
                   className={cn(
                     "w-full border border-gray-300 px-3 py-2",
-                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                   )}
                 />
               </div>
@@ -77,12 +83,35 @@ export default function NewTrip() {
                   name="endDate"
                   className={cn(
                     "w-full border border-gray-300 px-3 py-2",
-                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                   )}
                 />
               </div>
             </div>
+            <div>
+              <label> Trip Image</label>
 
+              {imageUrl && (
+                <Image
+                  src={imageUrl}
+                  alt="Trip Preview"
+                  className="w-full mb-4 rounded-md max-h-48 object-cover"
+                  width={300}
+                  height={100}
+                />
+              )}
+              <UploadButton
+                endpoint="imageUploader"
+                onClientUploadComplete={(res) => {
+                  if (res && res[0].ufsUrl) {
+                    setImageUrl(res[0].ufsUrl);
+                  }
+                }}
+                onUploadError={(error: Error) => {
+                  console.error("Upload error: ", error);
+                }}
+              />
+            </div>
             <Button type="submit" disabled={isPending} className="w-full">
               {isPending ? "Creating..." : "Create Trip"}
             </Button>
